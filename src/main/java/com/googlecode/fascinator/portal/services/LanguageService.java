ad4a14a5b7be376bd4a4fff6564a07019d8678f8
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2014-02-24 23:41:58 +1030

    Initial commit of source code moved from the-fascinator project. See the-fascinator project for code history.

---
+++ src/main/java/com/googlecode/fascinator/portal/services/LanguageService.java
@@ -0,0 +1,11 @@
+package com.googlecode.fascinator.portal.services;
+
+public interface LanguageService extends FascinatorService {
+
+    public abstract String displayMessage(String messageCode);
+
+    public abstract String displayMessage(String messageCode, String region);
+
+	public abstract void reloadLanguageFiles();
+
+}
